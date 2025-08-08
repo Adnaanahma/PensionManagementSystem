b20340826cece44e713eb98a33798784e32e99f5
Adnaanahma <adnanhashim1212@gmail.com>
2025-08-08 01:07:05 +0100

    validation check

--- PensionManagementSystem.Application/Validators/ContributionValidator.cs
+++ PensionManagementSystem.Application/Validators/ContributionValidator.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using FluentValidation;
 using PensionManagementSystem.Application.Dtos;
+using PensionManagementSystem.Application.Enums;
 
 namespace PensionManagementSystem.Application.Validators
 {
@@ -12,11 +13,38 @@ namespace PensionManagementSystem.Application.Validators
     {
         public ContributionDtoValidator()
         {
-            RuleFor(x => x.ContributionType).IsInEnum();
-            RuleFor(x => x.Amount).GreaterThan(0);
-            RuleFor(x => x.ContributionDate).NotEmpty();
+            RuleFor(x => x.ContributionType)
+            .IsInEnum()
+            .WithMessage("Invalid contribution type.");
+
+            RuleFor(x => x.Amount)
+                .GreaterThan(0)
+                .WithMessage("Contribution amount must be greater than zero.");
+
+            RuleFor(x => x.ContributionDate)
+            .NotEmpty()
+            .WithMessage("Contribution date is required.")
+            .Must((dto, date) => IsValidDate(dto.ContributionType, date))
+            .WithMessage("Invalid contribution date for the selected contribution type.");
+
             RuleFor(x => x.ReferenceNumber).NotEmpty().Matches("^[A-Z0-9-]+$");
-            RuleFor(x => x.MemberId).NotEmpty();
+
+            RuleFor(x => x.MemberId).NotEmpty().WithMessage("Member ID is required.");
+
+        }
+
+        private bool IsValidDate(ContributionType type, DateTime date)
+        {
+            if (type == ContributionType.Voluntary)
+                return true; // unrestricted
+
+            if (type == ContributionType.Monthly)
+            {
+                var now = DateTime.UtcNow;
+                return date.Year == now.Year && date.Month == now.Month;
+            }
+
+            return false;
         }
     }
 }
